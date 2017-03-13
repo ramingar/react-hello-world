3c3a36ca1224ac54881dd923b7ccc4719f5d1778
Rafael Minguet <rminguet@gmail.com>
2017-03-13 22:55:32 +0100

    Config for rollupjs

--- rollup-dev.config.js
+++ rollup-dev.config.js
@@ -1,26 +1,22 @@
 /**
  * Created by rafael on 13/03/17.
  */
-// rollup.config.js
-import  replace from 'rollup-plugin-replace';
+import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
 
 export default {
     entry: 'web/assets/src/js/init.js', // Entry file,
     format: 'es',
     plugins: [
         replace({
-            'process.env.NODE_ENV': JSON.stringify( 'development' )
+            'process.env.NODE_ENV': JSON.stringify('development')
         }),
         resolve(),
         commonjs(),
-        babel({
-            exclude: 'node_modules/**',
-            presets: ['es2015-rollup', 'react'],
-            plugins: ['external-helpers']
-        })
+        babel(babelrc({addModuleOptions: false}))
     ],
     dest: 'web/assets/build/js/index.js'
 };
